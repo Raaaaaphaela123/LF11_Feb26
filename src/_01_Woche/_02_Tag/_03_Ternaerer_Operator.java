ffc4af3143d480377a723e2ec15b271f9e16c471
Raphaela Dohrmann <raphaela.dohrmann@gfn.de>
2026-02-24 09:36:29 +0100

    Tag 2 Kontrollstrukturen

---
+++ src/_01_Woche/_02_Tag/_03_Ternaerer_Operator.java
@@ -0,0 +1,31 @@
+package _01_Woche._02_Tag;
+
+public class _03_Ternaerer_Operator {
+     static void main(String[] args) {
+        // Was ist der Konditionaloperator?
+        // Kurzform für einfache if-else-Entscheidungen:
+        // Syntax: Bedingung ? WertWennTrue : WertWennFalse
+
+        // Beispiel 1: Altersprüfung für Kauf bestimmter Produkte
+         int alter = 20;
+         String zugang = (alter >= 18) ? "Kauf erlaubt" : "Kauf verweigert";
+         IO.println("Person hat Zugang? " + zugang);
+
+        // Beispiel 2: Anzeige je nach Login-Status
+         boolean istEingeloggt = false;
+         String status = istEingeloggt ? "Logout anzeigen" : "Login anzeigen";
+         IO.println("Benutzerstatus: " + status);
+
+
+         boolean hatGeld = false;
+         // unschön, besser if-else
+         String status2 = istEingeloggt ? "Logout anzeigen" : (alter >= 18) ? "Ist 18" : hatGeld ? "Hat Geld" : "Kein Geld, kein Login, kein 18";
+
+
+         // Hinweis:
+        // Der Konditionaloperator ist ideal für einfache, übersichtliche Bedingungen,
+        // ersetzt jedoch keine komplexen if-Strukturen.
+
+
+    }
+}
