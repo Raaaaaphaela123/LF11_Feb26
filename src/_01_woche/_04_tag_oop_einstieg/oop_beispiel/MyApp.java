3a03a8b6043d3071e1f0b6fc06fc986424c67be3
Raphaela Dohrmann <raphaela.dohrmann@gfn.de>
2026-02-26 11:02:45 +0100

    OOP Einstieg - Kommentare

--- src/_01_woche/_04_tag_oop_einstieg/oop_beispiel/MyApp.java
+++ src/_01_woche/_04_tag_oop_einstieg/oop_beispiel/MyApp.java
@@ -2,18 +2,25 @@ package _01_woche._04_tag_oop_einstieg.oop_beispiel;
 
 public class MyApp {
     static void main() {
+        // Instanziierung eines konkreten Objektes
         Hund bello = new Hund();
+
+        // setzen der Instanzattribute
         bello.setName("Bello");
         bello.setLeinenLaenge("3 Meter");
+
         IO.println(bello.getName());
         IO.println(bello.getLeinenLaenge());
 
+        // Aufruf von überladener Methode aus der Klasse Hund
         bello.spiele();
         bello.spiele("wild");
 
+        // Aufruf der überschriebenen Methode
         bello.macheTon();
 
         IO.println("___");
+        // Dynamische Bindung -> folgt in LFZQ16a genauer!
         Tier meinHund = new Hund();
         meinHund.macheTon();
 
