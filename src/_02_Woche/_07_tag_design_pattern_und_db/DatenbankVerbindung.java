ec5aaa39a48bbd4f0e4477f7ce15522709996927
Raphaela Dohrmann <raphaela.dohrmann@gfn.de>
2026-03-03 14:22:03 +0100

    DB-Verbindung

---
+++ src/_02_Woche/_07_tag_design_pattern_und_db/DatenbankVerbindung.java
@@ -0,0 +1,48 @@
+package _02_Woche._07_tag_design_pattern_und_db;
+
+import java.sql.*;
+
+public class DatenbankVerbindung {
+
+    static void main() {
+
+        String dbUrl = "jdbc:mysql://localhost:3307/testdb";
+        String user = "root";
+        String password = "";
+
+        try {
+            Connection connection = DriverManager.getConnection(dbUrl, user, password);
+            IO.println("Verbindung erfolgreich!");
+
+            Statement statement = connection.createStatement();
+
+            // DELETE -> User löschen
+            String deleteSQL = "DELETE FROM users WHERE name = 'Maximilian'";
+            int rowsDeleted = statement.executeUpdate(deleteSQL);
+            IO.println("Rows deleted: " + rowsDeleted);
+
+
+            // SELECT -> vorhandene Daten anzeigen lassen
+            ResultSet resultSet = statement.executeQuery("SELECT id, name FROM users ORDER BY id");
+
+            // Iteration über die gelieferten Datensätze, bis die Liste kein nächstes Element mehr hat
+            while (resultSet.next()) {
+                IO.println("ID: " + resultSet.getInt("id") + ", Name: " + resultSet.getString("name"));
+            }
+
+            // INSERT -> neuen User hinzufügen
+            String insertSQL = "INSERT INTO users (name, password) VALUES ('Max', 'Geheim123!')";
+            statement.execute(insertSQL);
+
+            // UPDATE -> User verändern
+            String updateSQL = "UPDATE users SET name = 'Maximilian' WHERE name = 'Max'";
+            statement.executeQuery(updateSQL);
+
+            connection.close();
+
+        } catch (SQLException e) {
+            IO.println("Verbindung zur Datenbank fehlgeschlagen");
+        }
+
+    }
+}
